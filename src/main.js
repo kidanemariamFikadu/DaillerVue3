c82a34ac23dbf9e045d6f4eee85a48aa65e1df05
kidanemariam <kide.fikadu1@gmail.com>
2023-02-12 14:59:17 +0300

    removed unused libraries

--- src/main.js
+++ src/main.js
@@ -1,9 +1,3 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-library.add(fas);
-
-createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
+createApp(App).mount("#app");
